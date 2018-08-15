052bda34c9db18a23f7b2c4024522ff95b083333
TomSmaj <thomas.smajstrla@gmail.com>
2018-08-15 16:51:02 -0500

    images blurred and code transitioned to using  object array containing name and pics of each artist

--- assets/javascript/code.js
+++ assets/javascript/code.js
@@ -1,20 +1,51 @@
 
 (function(){
-//bank of words that player will try to guess
-let wordBank = ["modest mouse", "duster", "american football", "sebadoh", "the flaming lips", "galaxie 500", "pavement", "built to spill",
-                "digible planets", "outkast", "wu tang clan", "aphex twin", "bjork", "boards of canada", "slowdive", "my bloody valentine",
-                "flake music", "guided by voices", "dj shadow", "pj harvey", "weezer", "radiohead", "yo la tengo", "nirvana", "stereolab",
-                "pinback", "at the drive in", "they might be giants", "disco inferno", "polvo", "slint", "capn jazz", "elliott smith",
-                "the american analog set", "red hot chili peppers"];
-let fileName = {"modest mouse":"this-is-a-long-drive.jpg", "duster": "stratosphere-cover.jpg", "american football":"american-football.jpg",
-                "sebadoh":"sebadoh-iii.jpg", "the flaming lips":"soft-bulletin.jpg", "galaxie 500":"on-fire.jpg", "pavement":"slanted-and-enchanted.jpg", 
-                "built to spill":"keep-it-like-a-secret.jpg", "digible planets":"reachin.jpg", "outkast":"atliens.jpeg", "wu tang clan":"enter-the-wu-tang.jpg", 
-                "aphex twin":"richard-d-james.jpg", "bjork":"post.jpg", "boards of canada":"twoism.jpg", "slowdive":"souvlaki.jpg", "my bloody valentine":"loveless.jpg",
-                "flake music":"when-you-land.jpg", "guided by voices":"bee-thousand.png", "dj shadow":"endtroducing.jpg", "pj harvey":"rid-of-me.jpg", "weezer":"blue-album.jpg",
-                "radiohead":"ok-computer.jpg", "yo la tengo":"i-can-hear.jpg", "nirvana":"in-utero.jpg", "stereolab":"dots-and-loops.jpg", "pinback":"pinback.jpg",
-                "at the drive in":"relationship-of-command.jpg", "they might be giants":"lincoln.jpg", "disco inferno":"disco-inferno.jpg", "polvo":"exploded-drawing.jpg",
-                "slint":"spiderland.jpg", "capn jazz":"capn-jazz.jpg", "elliott smith":"either-or.jpg", "the american analog set":"know-by-heart.jpg", "red hot chili peppers":"blood-sugar-sex-magic.jpg"};
-                //associate list of letters and ASCII numbers
+
+                
+
+
+let artist = [
+    {name:"modest mouse", pic:"this-is-a-long-drive.jpg"},
+    {name:"duster", pic:"stratosphere-cover.jpg"},
+    {name:"american football", pic:"american-football.jpg"},
+    {name:"sebadoh", pic:"sebadoh-iii.jpg"},
+    {name:"the flaming lips", pic:"soft-bulletin.jpg"},
+    {name:"galaxie 500", pic:"on-fire.jpg"},
+    {name:"pavement", pic:"slanted-and-enchanted.jpg"}, 
+    {name:"built to spill", pic:"keep-it-like-a-secret.jpg"},
+    {name:"digible planets", pic:"reachin.jpg"},
+    {name:"outkast", pic:"atliens.jpeg"},
+    {name:"wu tang clan", pic:"enter-the-wu-tang.jpg"}, 
+    {name:"aphex twin", pic:"richard-d-james.jpg"},
+    {name:"bjork", pic:"post.jpg"},
+    {name:"boards of canada", pic:"twoism.jpg"},
+    {name:"slowdive", pic:"souvlaki.jpg"},
+    {name:"my bloody valentine", pic:"loveless.jpg"},
+    {name:"flake music", pic:"when-you-land.jpg"},
+    {name:"guided by voices", pic:"bee-thousand.png"},
+    {name:"dj shadow", pic:"endtroducing.jpg"},
+    {name:"pj harvey", pic:"rid-of-me.jpg"},
+    {name:"weezer", pic:"blue-album.jpg"},
+    {name:"radiohead", pic:"ok-computer.jpg"},
+    {name:"yo la tengo", pic:"i-can-hear.jpg"},
+    {name:"nirvana", pic:"in-utero.jpg"},
+    {name:"stereolab", pic:"dots-and-loops.jpg"},
+    {name:"pinback", pic:"pinback.jpg"},
+    {name:"at the drive in", pic:"relationship-of-command.jpg"},
+    {name:"they might be giants", pic:"lincoln.jpg"},
+    {name:"disco inferno", pic:"disco-inferno.jpg"},
+    {name:"polvo", pic:"exploded-drawing.jpg"},
+    {name:"slint", pic:"spiderland.jpg"},
+    {name:"capn jazz", pic:"capn-jazz.jpg"},
+    {name:"elliott smith", pic:"either-or.jpg"},
+    {name:"the american analog set", pic:"know-by-heart.jpg"},
+    {name:"red hot chili peppers", pic:"blood-sugar-sex-magic.jpg"},
+    {name:"cocteau twins", pic:"heaven-or-las-vegas.png"}
+];
+
+
+//associate list of letters and ASCII numbers
+//use event.key to avoid havving to use ASCII code
 let letters = { 0:48, 1:49, 2:50, 3:51, 4:52, 5:53, 6:54, 7:55, 8:56, 9:57, a:65, b:66, c:67, d:68, e:69, f:70, g:71, h:72,
                 i:73, j:74, k:75, l:76, m:77, n:78, o:79, p:80, q:81, r:82, s:83, t:84, u:85, v:86, w:87, x:88, y:89, z:90}
 //used to check if the initial screen has been set up 
@@ -26,16 +57,16 @@ let gWord = "";
 //will store each guess the user makes
 let guess = "";
 let output = "";
-let guessRemaining = 7, wins = 0;
+let guessRemaining = 8, wins = 0;
 let usedWords = "";
 let lettersInWord = "";
 
 document.addEventListener("keyup", function(event){
 
     if(!setup){
-        if(wordBank.length === 0){killScreen();}
+        if(artist.length === 0){killScreen();}
         //randomly pick guess Word
-        gWord = chooseWord();;
+        gWord = chooseWord();
         console.log(gWord);
         //create appropriate number of spaces on screen
         lettersInWord = genLetters(gWord);
@@ -65,7 +96,7 @@ const makeGuess = (gWord, guess, output) => {
     usedWords += guess;
     if(!(gWord.includes(guess))){
         guessRemaining--;
-        document.querySelector(".instructions").innerHTML = "The word does not contain: " + guess;
+        document.querySelector(".instructions").innerHTML = "The band name does not contain: " + guess;
         outputUsedWords(usedWords);
         document.querySelector(".wins-guesses").innerHTML = "Wins: " + wins + ", Guesses Remaining: " + guessRemaining;
         return;
@@ -89,18 +120,26 @@ const set = (gWord) => {
         if(gWord.charAt(i) === " "){underscores += "-- ";}
         else{underscores += "_ ";}
     }
-    for(key in fileName){
-        console.log("key: " + key);
-        console.log("gWord: " + gWord);
-        console.log("source: " + fileName[key]);
-        if(key === gWord){source += fileName[key];}
+    let found = false;
+    let j = 0;
+    while(!found){
+        if(artist[j].name === gWord){
+            source += artist[j].pic;
+            found = true;
+        }
+        else{j++;}
     }
+    
     document.querySelector(".album").innerHTML = "<img class = \"album-art\" src = \"" + source + "\" >";
     document.querySelector(".wins-guesses").innerHTML = "Wins: " + wins + ", Guesses Remaining: " + guessRemaining;
     document.querySelector(".used-words").innerHTML = "";  
     document.querySelector(".instructions").innerHTML = "";   
 
-    setup = true;  
+    setup = true; 
+
+    //after the guess word has been selected, and the album cover has been printed to the screen,
+    //the object containing the name and pic can be removed so it is not selected again
+    artist.splice(artist.indexOf(gWord), 1);
 
     underscores = underscores.split(" ");
     drawWord(underscores); 
@@ -138,13 +177,13 @@ const endGame = () => {
     if(correct){
         wins += 1;
         document.querySelector(".wins-guesses").innerHTML = "Wins: " + wins + ", Guesses Remaining: " + guessRemaining;
-        document.querySelector(".instructions").innerHTML = "You Win! Press any key to play another game.";  
+        document.querySelector(".instructions").innerHTML = "Correct! Press any key to keep playing.";  
     }
     else{
-        document.querySelector(".instructions").innerHTML = "Bummer, you lost :( ... Press any key to play another game.";  
+        document.querySelector(".instructions").innerHTML = "Bummer, you lost :( ... Press any key to keep playing.";  
     }
     setup = false;
-    guessRemaining = 7;
+    guessRemaining = 8;
     correct = false;
     gWord = "";
     guess = "";
@@ -173,15 +212,14 @@ const drawWord = (outP) => {
 }
 
 const chooseWord = () => {
-    let word = wordBank[Math.floor(Math.random() * wordBank.length)];
-    wordBank.splice(wordBank.indexOf(word), 1);
-    return word;
+    let word = artist[Math.floor(Math.random() * artist.length)];
+    return word.name;
 }
 
 const killScreen = () => {
-    document.querySelector(".wins-guesses").innerHTML = ""; 
+    document.querySelector(".wins-guesses").innerHTML = "Wins: " + wins; 
     document.querySelector(".unknown-word").innerHTML = "Wowee"; 
-    document.querySelector(".instructions").innerHTML = "There are no more words to guess!";   
+    document.querySelector(".instructions").innerHTML = "There are no more bands to guess!";   
     document.querySelector(".used-words").innerHTML = "You win!"; 
 
 }
